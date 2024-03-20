eb63eb7ec05c038a3c43eab32831319782789bca
whatcodehaveyougit <sigurdwatt@gmail.com>
2024-03-20 20:53:05 +0000

    Clear btn and filter display only when needed

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const input = document.getElementById("item-input")
 const form = document.getElementById('item-form')
 const list = document.getElementById('item-list')
-
-
+const clearBtn = document.getElementById('clear')
+const itemFilter = document.getElementById('filter')
 
 // functions
 
@@ -15,6 +15,7 @@ function addToDo(e){
   listItem.appendChild(button)
   list.appendChild(listItem)
   input.value = ''
+  checkUI()
 }
 
 function createDeleteButton(){
@@ -27,15 +28,27 @@ function createDeleteButton(){
 }
 
 function removeItem(e){
-  console.log('remove item');
-  console.log(e)
   if(e.target.parentElement.classList.contains('remove-item')){
     e.target.parentElement.parentElement.remove();
+    checkUI()
   }
 }
 
+function checkUI() {
+  const items = list.querySelectorAll('li')
+  if(items.length === 0){
+    clearBtn.style.display = 'none'
+    itemFilter.style.display = 'none'
+  } else {
+    clearBtn.style.display = 'block'
+    itemFilter.style.display = 'block'
+  }
+}
 
 // Listeners
 
 form.addEventListener('submit', addToDo)
 list.addEventListener('click', removeItem)
+document.addEventListener('DOMContentLoaded', () => {
+  checkUI()
+})
