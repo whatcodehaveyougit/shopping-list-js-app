48c2219df44069be740d2a31afcfbc2980da9927
whatcodehaveyougit <sigurdwatt@gmail.com>
2024-03-21 16:04:18 +0000

    Refactor to use classes / modules. COde much cleaner

---
+++ LocalStorage.js
@@ -0,0 +1,44 @@
+
+class LocalStorage {
+  constructor() {
+  }
+
+  addItemToStorage(item){
+    if(localStorage.getItem('items') === null){
+      localStorage.setItem('items', JSON.stringify([{ todo: item}]))
+    } else {
+      const items = JSON.parse(localStorage.getItem('items'))
+      items.push({todo: item})
+      localStorage.setItem('items', JSON.stringify(items))
+    }
+  }
+
+  doesItemAlreadyExist(item){
+    const items = localStorage.getItem('items')
+    let doesItemAlreadyExist = false
+    if(items !== null){
+      const itemsArr = JSON.parse(items)
+      itemsArr.forEach((element) => {
+        if(element.todo === item){
+          doesItemAlreadyExist = true
+        }
+      })
+    }
+    return doesItemAlreadyExist
+  }
+
+  removeItemFromStorage(item){
+    const items = JSON.parse(localStorage.getItem('items'))
+    items.forEach((element, index) => {
+      if(element.todo === item){
+        items.splice(index, 1)
+      }
+    })
+    localStorage.setItem('items', JSON.stringify(items))
+  }
+
+
+
+}
+
+export default LocalStorage;
---
+++ ToDo.js
@@ -0,0 +1,150 @@
+import LocalStorage from './LocalStorage.js'
+
+class ToDo {
+  constructor() {
+    console.log('ToDo class instantiated')
+    this.input = document.getElementById("item-input")
+    this.form = document.getElementById('item-form')
+    this.list = document.getElementById('item-list')
+    this.clearBtn = document.getElementById('clear')
+    this.itemFilter = document.getElementById('filter')
+    console.log(new LocalStorage)
+    this.localStorageObj = new LocalStorage()
+    this.startListeners()
+  }
+
+  startListeners() {
+    // Listeners
+    this.form.addEventListener('submit', this.addToDo.bind(this))
+    this.list.addEventListener('click', this.clickItem.bind(this))
+    this.clearBtn.addEventListener('click', this.clearItems.bind(this))
+    this.itemFilter.addEventListener('keyup', this.filterItems.bind(this))
+    document.addEventListener('DOMContentLoaded', () => {
+      this.loadItemsFromLocalStorage();
+      this.checkUI()
+    })
+  }
+
+  addItemToDOM(input){
+    if(input === ''){
+      alert('Please enter a task')
+    }
+    const listItem = document.createElement('li')
+    const text = document.createTextNode(input)
+    const button = this.createDeleteButton()
+    listItem.appendChild(text)
+    listItem.appendChild(button)
+    this.list.appendChild(listItem)
+  }
+
+  addToDo(e){
+    e.preventDefault();
+    console.log(this.localStorageObj)
+    if(this.localStorageObj.doesItemAlreadyExist(this.input.value)){
+      alert('Item already exists')
+    } else {
+      this.addItemToDOM(this.input.value)
+      this.localStorageObj.addItemToStorage(this.input.value)
+      this.input.value = ''
+      this.checkUI()
+    }
+  }
+
+  createDeleteButton(){
+    const btn = document.createElement('button')
+    btn.classList = 'remove-item btn-link text-red'
+    const icon = document.createElement('icon')
+    icon.classList = 'fa-solid fa-xmark'
+    btn.appendChild(icon)
+    return btn;
+  }
+
+  didUserClickDelete(e){
+    if (e.target.parentElement.classList.contains('remove-item')){
+      return true
+    }
+    return false
+  }
+
+  clickItem(e){
+    if(e.target.localName == 'li'){
+      this.setItemToEdit(e.target)
+    } else if(this.didUserClickDelete(e)){
+      this.localStorageObj.removeItemFromStorage(e.target.parentElement.parentElement.firstChild.textContent)
+      e.target.parentElement.parentElement.remove();
+      this.checkUI()
+    }
+  }
+
+  convertToInput(element) {
+    const inputElement = document.createElement('input');
+    inputElement.value = element.textContent;
+    element.replaceWith(inputElement);
+    inputElement.focus();
+    inputElement.addEventListener('blur', (e) => this.convertToLi(e.target))
+  }
+
+  convertToLi(inputElement) {
+    console.log('converting')
+    console.log(inputElement)
+    const liElement = document.createElement('li')
+    const text = document.createTextNode(inputElement.value)
+    const button = this.createDeleteButton()
+    inputElement.replaceWith(liElement);
+    liElement.appendChild(text)
+    liElement.appendChild(button)
+  }
+
+  // Clear and Filter itesms
+
+  checkUI() {
+    const items = this.list.querySelectorAll('li')
+    if(items.length === 0){
+      this.clearBtn.style.display = 'none'
+      this.itemFilter.style.display = 'none'
+    } else {
+      this.clearBtn.style.display = 'block'
+      this.itemFilter.style.display = 'block'
+    }
+  }
+
+  clearItems(){
+    alert('Are you sure you want to clear all items?')
+    this.list.innerHTML = ''
+    localStorage.clear('items')
+    this.checkUI()
+  }
+
+  filterItems(e){
+    const text = e.target.value.toLowerCase()
+    document.querySelectorAll('li').forEach((item) => {
+      const task = item.firstChild.textContent.toLowerCase()
+      if(task.indexOf(text) != -1){
+        item.style.display = 'flex'
+      } else {
+        item.style.display = 'none'
+      }
+    })
+  }
+
+  setItemToEdit(item){
+    this.localStorageObj.removeItemFromStorage(item)
+    this.convertToInput(item)
+  }
+
+  loadItemsFromLocalStorage(){
+    if(localStorage.getItem('items') !== null){
+      const items = JSON.parse(localStorage.getItem('items'))
+      items.forEach((item) => {
+        const listItem = document.createElement('li')
+        const text = document.createTextNode(item.todo)
+        const button = this.createDeleteButton()
+        listItem.appendChild(text)
+        listItem.appendChild(button)
+        this.list.appendChild(listItem)
+      })
+    }
+  }
+}
+
+export default ToDo
